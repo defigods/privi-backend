5b342bbad0517564900ab8b4008b5310dec980f4
defigods <defigodss@gmail.com>
2020-10-24 13:02:44 +0200

    Backend on SSL

--- src/controllers/server.ts
+++ src/controllers/server.ts
@@ -13,47 +13,80 @@ const lendingRoutes = require('../routes/lendingRoutes.ts');
 const walletRoutes = require('../routes/walletRoutes.ts');
 const profileRoutes = require('../routes/profileRoutes.ts');
 const priviScanRoutes = require('../routes/priviScanRoutes');
+type Env = 'dev' | 'prod';
 
-export const startServer = () => {
-    // initialize configuration
-    //dotenv.config();
+export const startServer = (env: Env) => {
+  // initialize configuration
+  //dotenv.config();
 
-    const port = 3001;
-    const app = express();
+  const port = 3000;
+  const app = express();
 
-    app.use(cors());
-    // Set HTTP headers for security
-    app.use(helmet());
+  app.use(cors());
+  // Set HTTP headers for security
+  app.use(helmet());
 
-    // Configure Express to parse incoming JSON data
-    app.use(express.json());
+  // Configure Express to parse incoming JSON data
+  app.use(express.json());
 
-    // Configure Express to use EJS
-    app.set("views", path.join(__dirname, "views"));
+  // Configure Express to use EJS
+  app.set("views", path.join(__dirname, "views"));
 
-    // Routes definition
-    app.use('/user', userRoutes);
-    app.use('/pod', podRoutes);
-    app.use('/stake', stakeRoutes);
-    app.use('/lendings', lendingRoutes);
-    app.use('/wallet', walletRoutes);
-    app.use('/profile', profileRoutes);
-    app.use('/privi-scan', priviScanRoutes);
+  // Routes definition
+  app.use('/user', userRoutes);
+  app.use('/pod', podRoutes);
+  app.use('/stake', stakeRoutes);
+  app.use('/lendings', lendingRoutes);
+  app.use('/wallet', walletRoutes);
+  app.use('/profile', profileRoutes);
+  app.use('/privi-scan', priviScanRoutes);
 
-    // cron job for generating 
+  // cron job for generating 
 
-    https.createServer({
-        key: fs.readFileSync('server.key'),
-        cert: fs.readFileSync('server.cert')
-      }, app)
-      .listen(3000, function () {
-        console.log(`server started at http://localhost:${port}`)
-      })
 
-    // start the express server
-    /*app.listen(port, () => {
-        // tslint:disable-next-line:no-console
-        console.log(`server started at http://localhost:${port}`);
-    });*/
+  // Start server
+  switch (env) {
+    case 'dev':
+      app.listen(port);
+      console.log(`Back-end (Non-SSL) running on port ${port}`);
+      break;
+    case 'prod':
+        try {
+            const privateKey = fs.readFileSync('/etc/letsencrypt/live/priviweb.tech/privkey.pem', 'utf8');
+            const certificate = fs.readFileSync('/etc/letsencrypt/live/priviweb.tech/cert.pem', 'utf8');
+            const ca = fs.readFileSync('/etc/letsencrypt/live/priviweb.tech/chain.pem', 'utf8');
+            const credentials = {
+                key: privateKey,
+                cert: certificate,
+                ca: ca
+            };
+            const httpsServer = https.createServer(credentials, app);
+            httpsServer.listen(port, () => {
+                console.log(`Back-end (SSL) running on port ${port}`);
+            });
+        } catch (err) {
+            console.log('Certificate not found!', err);
+        }
+      break;
+    default:
+      console.log(`Invalid environment ('dev' or 'prod' not found)`);
+      break;
+  }
+
+  /*
+  https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+  }, app)
+    .listen(3000, function () {
+      console.log(`server started at http://localhost:${port}`)
+    });
+*/
+
+  // start the express server
+  /*app.listen(port, () => {
+      // tslint:disable-next-line:no-console
+      console.log(`server started at http://localhost:${port}`);
+  });*/
 };
 
