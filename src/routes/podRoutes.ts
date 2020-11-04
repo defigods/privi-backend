f1f21e102605de79b382d9cfea310abadf71c511
defigods <defigodss@gmail.com>
2020-11-04 17:15:11 +0100

    Pods Getters updated

--- src/routes/podRoutes.ts
+++ src/routes/podRoutes.ts
@@ -3,6 +3,13 @@ const router = express.Router();
 
 const podController = require('../controllers/podController');
 
+router.get('/NFT/getMyPods/:userId', podController.getMyPodsNFT);
+router.get('/FT/getMyPods/:userId', podController.getMyPodsFT);
+router.get('/NFT/getTrendingPods/:userId', podController.getTrendingPodsNFT);
+router.get('/FT/getTrendingPods/:userId', podController.getTrendingPodsFT);
+router.get('/NFT/getOtherPods/:userId', podController.getOtherPodsNFT);
+router.get('/FT/getOtherPods/:userId', podController.getOtherPodsFT);
+
 router.post('/initiatePod', podController.initiatePOD);
 router.post('/deletePod', podController.deletePOD);
 router.post('/investPod', podController.investPOD);
