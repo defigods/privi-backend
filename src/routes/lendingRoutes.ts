016be407ed704377247c2e459ec3338e475166ca
defigods <defigodss@gmail.com>
2020-11-06 20:33:48 +0000

    lending getter

--- src/routes/lendingRoutes.ts
+++ src/routes/lendingRoutes.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const lendingController = require('../controllers/lendingController');
 
+router.post('/getUserLoans', lendingController.getUserLoans);
 router.post('/borrowFunds', lendingController.borrowFunds);
 router.post('/depositCollateral', lendingController.depositCollateral);
 router.post('/withdrawCollateral', lendingController.withdrawCollateral);
