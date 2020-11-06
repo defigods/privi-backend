e42f2a282fc94adeb88d7b1a06a70cd99626be45
defigods <defigodss@gmail.com>
2020-11-06 16:06:35 +0100

    Connect revision

--- src/routes/ethereumRoutes.ts
+++ src/routes/ethereumRoutes.ts
@@ -1,13 +1,12 @@
 
 import express from 'express';
 const router = express.Router();
-
 const connectController = require('../controllers/connectController');
 
-//router.post('/send', connectController.send);
 router.post('/transferEthWithdraw', connectController.transferEthWithdraw);
+//router.post('/balanceToken', connectController.balanceToken);
+//router.post('/transferTokenWithdraw', connectController.transferTokenWithdraw);
+//router.post('/send', connectController.send);
 //router.post('/swap', connectController.swap);
-router.post('/transferTokenWithdraw', connectController.transferTokenWithdraw);
-router.post('/balanceToken', connectController.balanceToken);
 
 module.exports = router;
