e9165e12aa148d8b3d915825990e606a77a11439
defigods <defigodss@gmail.com>
2020-11-10 04:43:44 -0600

    jwt for wallet routes

--- src/routes/walletRoutes.ts
+++ src/routes/walletRoutes.ts
@@ -1,20 +1,21 @@
 import express from 'express';
 const router = express.Router();
 
+import { authenticateJWT } from '../middlewares/jwtAuthMiddleware';
 const walletController = require('../controllers/walletController');
 
-router.post('/send', walletController.send);
-router.post('/withdraw', walletController.withdraw);
-router.post('/swap', walletController.swap);
+router.post('/send', authenticateJWT, walletController.send);
+router.post('/withdraw', authenticateJWT, walletController.withdraw);
+router.post('/swap', authenticateJWT, walletController.swap);
 
-// router.post('/getTokensRate', walletController.getTokensRate);
-router.get('/getTokensRate', walletController.getTokensRate);
-router.get('/getTotalBalance', walletController.getTotalBalance);
-router.get('/getTotalBalancePC', walletController.getTotalBalancePC);
-router.get('/getTokenBalances', walletController.getTokenBalances);
-router.get('/getTransfers', walletController.getTransfers);
-router.get('/getTransactions', walletController.getTransactions);
-router.get('/getTotalIncome', walletController.getTotalIncome);
-router.get('/getTotalExpense', walletController.getTotalExpense);
+// router.post('/getTokensRate', authenticateJWT, walletController.getTokensRate);
+router.get('/getTokensRate', authenticateJWT, walletController.getTokensRate);
+router.get('/getTotalBalance', authenticateJWT, walletController.getTotalBalance);
+router.get('/getTotalBalancePC', authenticateJWT, walletController.getTotalBalancePC);
+router.get('/getTokenBalances', authenticateJWT, walletController.getTokenBalances);
+router.get('/getTransfers', authenticateJWT, walletController.getTransfers);
+router.get('/getTransactions', authenticateJWT, walletController.getTransactions);
+router.get('/getTotalIncome', authenticateJWT, walletController.getTotalIncome);
+router.get('/getTotalExpense', authenticateJWT, walletController.getTotalExpense);
 
 module.exports = router;
