c1c20a90d8bc5751ceb7539006b6043a2c7a7650
defigods <defigodss@gmail.com>
2020-11-11 15:01:00 +0100

    Added WETH

--- src/routes/ethereumRoutes.ts
+++ src/routes/ethereumRoutes.ts
@@ -4,10 +4,11 @@ const router = express.Router();
 const connectController = require('../controllers/connectController');
 
 router.get('/getERC20Balance', connectController.getERC20Balance);
+router.post('/swapERC20', connectController.swapERC20);
+router.post('/withdrawERC20', connectController.withdrawERC20);
 //router.post('/transferEthWithdraw', connectController.transferEthWithdraw);
 //router.post('/balanceToken', connectController.balanceToken);
 //router.post('/transferTokenWithdraw', connectController.transferTokenWithdraw);
-//router.post('/send', connectController.send);
-//router.post('/swap', connectController.swap);
+
 
 module.exports = router;
