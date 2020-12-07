b8add21ce00a84497298d07c92437b47101de3e6
defigods <defigodss@gmail.com>
2020-12-07 15:34:34 +0000

    PriviCredit update

--- src/blockchain/priviLending.js
+++ src/blockchain/priviLending.js
@@ -1,114 +1,100 @@
 const axios = require("axios");
 const api = require("./blockchainApi");
 
-module.exports.initiatePRIVIcredit = async (creator, amount, token, duration, payments, maxFunds, interest, p_incentive, p_premium, trustScore, endorsementScore) => {
-    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/initiatePRIVIcredit", {
-        Loan_conditions: {
-            Creator: creator,
-            Token: token,
-            Duration: duration,
-            Payments: payments,
-            MaxFunds: maxFunds,
-            Interest: interest,
-            P_incentive: p_incentive,
-            P_premium: p_premium,
-            TrustScore: trustScore,
-            EndorsementScore: endorsementScore,
-        },
-        InitialDeposit: String(amount),
+module.exports.getPRIVICreditInfo = async (creditAddress) => {
+    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/getPRIVICreditInfo", {
+            CreditAddress: creditAddress,
     }
     );
     return blockchainRes.data;
 };
 
-module.exports.modifyPRIVIparameters = async (creator, loanId, duration, payments, maxFunds, interest, p_incentive, p_premium, trustScore, endorsementScore) => {
-    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/modifyPRIVIparameters", {
-        loanId: loanId,
-        Creator: creator,
-        Duration: duration,
-        Payments: payments,
-        MaxFunds: maxFunds,
-        Interest: interest,
-        P_incentive: p_incentive,
-        P_premium: p_premium,
-        TrustScore: trustScore,
-        EndorsementScore: endorsementScore,
-    });
+module.exports.getPRIVICreditState = async (creditAddress) => {
+    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/getPRIVICreditState", {
+            CreditAddress: creditAddress,
+    }
+    );
     return blockchainRes.data;
 };
 
-
-module.exports.updateRiskParameters = async (token, interest_min, interest_max, p_incentive_min, p_incentive_max, p_premium_min, p_premium_max) => {
-    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/updateRiskParameters", {
-        Token: token,
-        RiskParameters: {
-            Interest_min: interest_min,
-            Interest_max: interest_max,
-            P_incentive_min: p_incentive_min,
-            P_incentive_max: p_incentive_max,
-            P_premium_min: p_premium_min,
-            P_premium_max: p_premium_max,
-        }
-    });
+module.exports.getUserLendings = async (address) => {
+    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/getUserLendings", {
+            Address: address,
+    }
+    );
     return blockchainRes.data;
 };
 
-module.exports.getPRIVIcreditList = async () => {
-    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/getPRIVIcreditList", {
-    });
+module.exports.getUserBorrowings = async (address) => {
+    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/getUserBorrowings", {
+            Address: address,
+    }
+    );
     return blockchainRes.data;
 };
 
-module.exports.getPRIVIcredit = async (loanId) => {
-    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/getPRIVIcredit", {
-        LoanId: loanId
-    });
+module.exports.getCreditBorrowers = async (creditAddress) => {
+    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/getCreditBorrowers", {
+            CreditAddress: creditAddress,
+    }
+    );
     return blockchainRes.data;
 };
 
-module.exports.depositFunds = async (loanId, lenderId, amount) => {
+module.exports.getCreditLenders = async (creditAddress) => {
+    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/getCreditLenders", {
+            CreditAddress: creditAddress,
+    }
+    );
+    return blockchainRes.data;
+};
+
+module.exports.initiatePRIVIcredit = async (creator, creditName, creditAddress, lendingToken, maxFunds, interest, frequency, p_incentive, p_premium, date, dateExpiration, trustScore, endorsementScore, collateralsAccepted, ccr, initialDeposit, txnId) => {
+    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/initiatePRIVIcredit", {
+        Loan_conditions: {
+            Creator: creator,
+            CreditName: creditName,
+            CreditAddress: creditAddress,
+            LendingToken: lendingToken,
+            MaxFunds: maxFunds,
+            Interest: interest,
+            Frequency: frequency,
+            P_incentive: p_incentive,
+            P_premium: p_premium,
+            Date: date,
+            DateExpiration: dateExpiration,
+            TrustScore: trustScore,
+            EndorsementScore: endorsementScore,
+            CollateralsAccepted: collateralsAccepted,
+            CCR: ccr,
+            InitialDeposit: initialDeposit,
+            TxnId: txnId,
+        },
+    }
+    );
+    return blockchainRes.data;
+};
+
+module.exports.depositFunds = async (creditAddress, address, amount, date, txnId) => {
     let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/depositFunds", {
-        LoanId: loanId,
-        LenderId: lenderId,
-        Amount: amount
-    });
-    return blockchainRes.data;
-};
-
-module.exports.withdrawFunds = async (loanId, userId, amount) => {
-    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/withdrawFunds", {
-        LoanId: loanId,
-        UserId: userId,
-        Amount: amount
-    });
-    return blockchainRes.data;
-};
-
-module.exports.borrowFunds = async (loanId, borrowerId, amount, collaterals) => {
-    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/borrowFunds", {
-        LoanId: loanId,
-        BorrowerId: borrowerId,
+        CreditAddress: creditAddress,
+        Address: address,
         Amount: amount,
-        Collaterals: collaterals
+        Date: date,
+        TxnId: txnId
     });
     return blockchainRes.data;
 };
 
-module.exports.assumePRIVIrisk = async (loanId, provider, premiumId, riskPct) => {
-    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/assumePRIVIrisk", {
-        LoanId: loanId,
-        ProviderId: provider,
-        PremiumId: premiumId,
-        Pct_Risk: riskPct,
+module.exports.borrowFunds = async (creditAddress, address, amount, date, txnId, collaterals, rateChange) => {
+    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/borrowFunds", {
+        CreditAddress: creditAddress,
+        Address: address,
+        Amount: amount,
+        Date: date,
+        TxnId: txnId,
+        Collaterals: collaterals,
+        RateChange: rateChange
     });
     return blockchainRes.data;
 };
-
-
-module.exports.managePRIVIcredits = async () => {
-    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/managePRIVIcredits", {});
-    return blockchainRes.data;
-};
-
-
-
--- src/routes/priviCreditRoutes.ts
+++ src/routes/priviCreditRoutes.ts
@@ -4,11 +4,14 @@ const router = express.Router();
 import { authenticateJWT } from '../middlewares/jwtAuthMiddleware';
 const priviCreditController = require('../controllers/priviCreditController');
 
-router.post('/initiateCredit', authenticateJWT, priviCreditController.initiateCredit);
-router.post('/modifyParameters', authenticateJWT, priviCreditController.modifyParameters);
-router.post('/withdrawFunds', authenticateJWT, priviCreditController.withdrawFunds);
-router.post('/borrowFunds', authenticateJWT, priviCreditController.borrowFunds);
+router.post('/getPRIVICreditInfo', authenticateJWT, priviCreditController.getPRIVICreditInfo);
+router.post('/getPRIVICreditState', authenticateJWT, priviCreditController.getPRIVICreditState);
+router.post('/getUserLendings', authenticateJWT, priviCreditController.getUserLendings);
+router.post('/getUserBorrowings', authenticateJWT, priviCreditController.getUserBorrowings);
+router.post('/getCreditBorrowers', authenticateJWT, priviCreditController.getCreditBorrowers);
+router.post('/getCreditLenders', authenticateJWT, priviCreditController.getCreditLenders);
+router.post('/initiatePriviCredit', authenticateJWT, priviCreditController.initiatePriviCredit);
 router.post('/depositFunds', authenticateJWT, priviCreditController.depositFunds);
-router.post('/getPriviCredits', authenticateJWT, priviCreditController.getPriviCredits);
+router.post('/borrowFunds', authenticateJWT, priviCreditController.borrowFunds);
 
 module.exports = router;
