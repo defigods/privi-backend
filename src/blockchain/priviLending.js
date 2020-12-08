826ea4c0ac89c967c2f0f9b29712c0b8aea54cd5
defigods <defigodss@gmail.com>
2020-12-08 17:30:54 +0000

    Added apiKey

--- src/blockchain/priviLending.js
+++ src/blockchain/priviLending.js
@@ -1,57 +1,63 @@
 const axios = require("axios");
 const api = require("./blockchainApi");
 
-module.exports.getPRIVICreditInfo = async (creditAddress) => {
+module.exports.getPRIVICreditInfo = async (creditAddress, caller) => {
     let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/getPRIVICreditInfo", {
             CreditAddress: creditAddress,
+            Caller: caller
     }
     );
     return blockchainRes.data;
 };
 
-module.exports.getPRIVICreditState = async (creditAddress) => {
+module.exports.getPRIVICreditState = async (creditAddress, caller) => {
     let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/getPRIVICreditState", {
             CreditAddress: creditAddress,
+            Caller: caller
     }
     );
     return blockchainRes.data;
 };
 
-module.exports.getUserLendings = async (address) => {
+module.exports.getUserLendings = async (address, caller) => {
     let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/getUserLendings", {
             Address: address,
+            Caller: caller
     }
     );
     return blockchainRes.data;
 };
 
-module.exports.getUserBorrowings = async (address) => {
+module.exports.getUserBorrowings = async (address, caller) => {
     let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/getUserBorrowings", {
             Address: address,
+            Caller: caller
     }
     );
     return blockchainRes.data;
 };
 
-module.exports.getCreditBorrowers = async (creditAddress) => {
+module.exports.getCreditBorrowers = async (creditAddress, caller) => {
     let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/getCreditBorrowers", {
             CreditAddress: creditAddress,
+            Caller: caller
     }
     );
     return blockchainRes.data;
 };
 
-module.exports.getCreditLenders = async (creditAddress) => {
+module.exports.getCreditLenders = async (creditAddress, caller) => {
     let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/getCreditLenders", {
             CreditAddress: creditAddress,
+            Caller: caller
     }
     );
     return blockchainRes.data;
 };
 
-module.exports.initiatePRIVIcredit = async (creator, creditName, creditAddress, lendingToken, maxFunds, interest, frequency, p_incentive, p_premium, date, dateExpiration, trustScore, endorsementScore, collateralsAccepted, ccr, initialDeposit, txnId) => {
+module.exports.initiatePRIVIcredit = async (creator, creditName, creditAddress, lendingToken, maxFunds, interest, frequency, p_incentive, p_premium, date, dateExpiration, trustScore, endorsementScore, collateralsAccepted, ccr, initialDeposit, txnId, caller) => {
     let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/initiatePRIVIcredit", {
-        Loan_conditions: {
+        Parameters: {
             Creator: creator,
             CreditName: creditName,
             CreditAddress: creditAddress,
@@ -62,39 +68,55 @@ module.exports.initiatePRIVIcredit = async (creator, creditName, creditAddress, 
             P_incentive: p_incentive,
             P_premium: p_premium,
             Date: date,
-            DateExpiration: dateExpiration,
+            DateExpiration: dateExpiration
+        },
+        Requirements: {
             TrustScore: trustScore,
             EndorsementScore: endorsementScore,
             CollateralsAccepted: collateralsAccepted,
-            CCR: ccr,
+            CCR: ccr 
+        },
+        Initialisation:{
             InitialDeposit: initialDeposit,
             TxnId: txnId,
         },
+        Caller: caller
     }
     );
     return blockchainRes.data;
 };
 
-module.exports.depositFunds = async (creditAddress, address, amount, date, txnId) => {
+module.exports.depositFunds = async (creditAddress, address, amount, date, txnId, caller) => {
     let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/depositFunds", {
         CreditAddress: creditAddress,
         Address: address,
         Amount: amount,
         Date: date,
-        TxnId: txnId
+        TxnId: txnId,
+        Caller: caller
     });
     return blockchainRes.data;
 };
 
-module.exports.borrowFunds = async (creditAddress, address, amount, date, txnId, collaterals, rateChange) => {
+module.exports.borrowFunds = async (creditAddress, address, amount, date, txnId, ethCollateral, ethRateChange, caller) => {
     let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/borrowFunds", {
         CreditAddress: creditAddress,
         Address: address,
         Amount: amount,
         Date: date,
         TxnId: txnId,
-        Collaterals: collaterals,
-        RateChange: rateChange
+        Collaterals: {
+            ETH: ethCollateral
+        },
+        RateChange: {
+            ETH: ethRateChange
+        },
+        Caller: caller
     });
+    return blockchainRes.data;
+};
+
+module.exports.managePRIVIcredits = async () => {
+    let blockchainRes = await axios.post(api.blockchainPriviLendingAPI + "/managePRIVIcredits", {});
     return blockchainRes.data;
 };
--- src/controllers/priviCreditController.ts
+++ src/controllers/priviCreditController.ts
@@ -6,6 +6,8 @@ import cron from 'node-cron';
 import { db } from '../firebase/firebase';
 import collections from '../firebase/collections';
 
+require('dotenv').config();
+const apiKey = "PRIVI"; // just for now
 
 exports.getPRIVICreditInfo = async (req: express.Request, res: express.Response) => {
     try {
@@ -28,9 +30,10 @@ exports.getPRIVICreditState = async (req: express.Request, res: express.Response
         const body = req.body;
         const creditAddress = body.creditAddress;
         const creditSnap = await db.collection(collections.priviCredits).doc(creditAddress).get();
-        const creditState = creditSnap.state;
+        const creditData = creditSnap.data();
         
-        if (creditState) {
+        if (creditData) {
+            const creditState = creditData.State.state;
             res.send({ success: true, data: creditState });
         }
     } catch (err) {
@@ -45,9 +48,9 @@ exports.getUserLendings = async (req: express.Request, res: express.Response) =>
         const address = body.address;
         const userSnap = await db.collection(collections.user).doc(address).get()
         const userData = userSnap.data();
-        const userLendings = userData.lendings;
         
-        if (userLendings) {
+        if (userData) {
+            const userLendings = userData.lendings;
             res.send({ success: true, data: userLendings });
         }
     } catch (err) {
@@ -62,9 +65,9 @@ exports.getUserBorrowings = async (req: express.Request, res: express.Response) 
         const address = body.address;
         const userSnap = await db.collection(collections.user).doc(address).get()
         const userData = userSnap.data();
-        const userBorrowings = userData.borrowings;
         
-        if (userBorrowings) {            
+        if (userData) {            
+            const userBorrowings = userData.borrowings;
             res.send({ success: true, data: userBorrowings });
         }
     } catch (err) {
@@ -79,9 +82,9 @@ exports.getCreditBorrowers = async (req: express.Request, res: express.Response)
         const creditAddress = body.creditAddress;
         const creditSnap = await db.collection(collections.priviCredits).doc(creditAddress).get();
         const creditData = creditSnap.data();
-        const creditBorrowers = creditData.borrowers;
         
-        if (creditBorrowers) {
+        if (creditData) {
+            const creditBorrowers = creditData.borrowers;
             res.send({ success: true, data: creditBorrowers });
         }
     } catch (err) {
@@ -96,9 +99,9 @@ exports.getCreditLenders = async (req: express.Request, res: express.Response) =
         const creditAddress = body.creditAddress;
         const creditSnap = await db.collection(collections.priviCredits).doc(creditAddress).get();
         const creditData = creditSnap.data();
-        const creditLenders = creditData.lenders;
         
-        if (creditLenders) {
+        if (creditData) {
+            const creditLenders = creditData.lenders;
             res.send({ success: true, data: creditLenders });
         }
     } catch (err) {
@@ -128,12 +131,13 @@ exports.initiatePriviCredit = async (req: express.Request, res: express.Response
         const ccr = body.ccr;
         const initialDeposit = body.initialDeposit;
         const txnId = body.txnId;
+        const caller = apiKey;
 
-        const blockchainRes = await priviCredit.initiatePRIVIcredit(creator, creditName, creditAddress, lendingToken, maxFunds, interest, frequency, p_incentive, p_premium, date, dateExpiration, trustScore, endorsementScore, collateralsAccepted, ccr, initialDeposit, txnId);
+        const blockchainRes = await priviCredit.initiatePRIVIcredit(creator, creditName, creditAddress, lendingToken, maxFunds, interest, frequency, p_incentive, p_premium, date, dateExpiration, trustScore, endorsementScore, collateralsAccepted, ccr, initialDeposit, txnId, caller);
         if (blockchainRes && blockchainRes.success) {
             updateFirebase(blockchainRes);
             createNotification(creator, "Privi Credit - Loan Offer Created",
-                `You have successfully created a new PRIVI Credit. ${amount} ${token} has been added to the PRIVI Credit Pool!`,
+                `You have successfully created a new PRIVI Credit. ${initialDeposit} ${lendingToken} has been added to the PRIVI Credit Pool!`,
                 notificationTypes.priviCreditCreated
             );
             const updateLoans = blockchainRes.output.UpdateLoans;
@@ -160,7 +164,9 @@ exports.depositFunds = async (req: express.Request, res: express.Response) => {
         const amount = body.amount;
         const date = body.date;
         const txnId = body.txnId;
-        const blockchainRes = await priviCredit.depositFunds(creditAddress, address, amount, date, txnId);
+        const caller = apiKey;
+
+        const blockchainRes = await priviCredit.depositFunds(creditAddress, address, amount, date, txnId, caller);
         if (blockchainRes && blockchainRes.success) {
             updateFirebase(blockchainRes);
             createNotification(address, "Privi Credit - Credit Deposited",
@@ -189,7 +195,9 @@ exports.borrowFunds = async (req: express.Request, res: express.Response) => {
         const txnId = body.txnId;
         const collaterals = body.collaterals;
         const rateChange = body.rateChange;
-        const blockchainRes = await priviCredit.borrowFunds(creditAddress, address, amount, date, txnId, collaterals, rateChange);
+        const caller = apiKey;
+
+        const blockchainRes = await priviCredit.borrowFunds(creditAddress, address, amount, date, txnId, collaterals, rateChange, caller);
         if (blockchainRes && blockchainRes.success) {
             updateFirebase(blockchainRes);
             createNotification(address, "Privi Credit - Loan Borrowed",
@@ -207,3 +215,33 @@ exports.borrowFunds = async (req: express.Request, res: express.Response) => {
         res.send({ success: false });
     }
 };
+
+/////////////////////////// CRON JOBS //////////////////////////////
+
+// scheduled every day at 00:00
+exports.managePRIVIcredits = cron.schedule('0 0 * * *', async () => {
+    try {
+        console.log("******** Privi Credit managePRIVIcredits ********");
+        const blockchainRes = await priviCredit.managePRIVIcredits();
+        if (blockchainRes && blockchainRes.success) {
+            console.log("******** Privi Credit managePRIVIcredits finished ********");
+            updateFirebase(blockchainRes);
+            const updateWallets = blockchainRes.output.UpdateWallets;
+            let uid: string = "";
+            let walletObj: any = null;
+            for ([uid, walletObj] of Object.entries(updateWallets)) {
+                if (walletObj["Transaction"].length > 0) {
+                    createNotification(uid, "Privi Credit - Interest Payment",
+                        ` `,
+                        notificationTypes.priviCreditInterest
+                    );
+                }
+            }
+        }
+        else {
+            console.log('Error in controllers/priviCredit -> managePRIVIcredits(): success = false');
+        }
+    } catch (err) {
+        console.log('Error in controllers/priviCredit -> managePRIVIcredits()', err);
+    }
+});
