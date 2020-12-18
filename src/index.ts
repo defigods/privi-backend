d2139a5c130098d5cd4844b640b1745cb10e3490
defigods <defigodss@gmail.com>
2020-12-18 03:09:14 -0600

    working socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { startServer } from './controllers/serverController';
+import { startServer, startSocket } from './controllers/serverController';
 
 // 'dev' => Development environment without SSL
 // 'devssl' => Development environment with SSL
@@ -8,10 +8,13 @@ const env: string = process.argv[2];
 const main = async () => {
 
     // Start Server
-    (env === 'dev' || env === 'prod' || env === 'devssl')
-    ? startServer(env)
-    : console.log(`Please use any of the following commands:\n$ nodemon dev => for Development (without SSL) \n$ nodemon devssl (with SSL)=> for Development \n$ nodemon prod => for Production`);
+    if (env === 'dev' || env === 'prod' || env === 'devssl') {
+        startServer(env);
+        startSocket(env);
 
+    } else {
+        console.log(`Please use any of the following commands:\n$ nodemon dev => for Development (without SSL) \n$ nodemon devssl (with SSL)=> for Development \n$ nodemon prod => for Production`);
+    }
 };
 
 main();
