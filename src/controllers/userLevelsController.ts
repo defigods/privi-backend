9cd7ea219a16b874608cea6a126778880d1c1e52
defigods <defigodss@gmail.com>
2021-01-18 17:35:31 +0300

    changed from (req, res) to promises

--- src/controllers/userLevelsController.ts
+++ src/controllers/userLevelsController.ts
@@ -24,196 +24,168 @@ exports.getLevelsInfo = async (req: express.Request, res: express.Response) => {
     }
 }
 
-exports.sumPoints = async (req: express.Request, res: express.Response) => {
-    try {
-        const userId = req.body.userId;
-        const numberOfPoints = req.body.numberOfPoints;
-        const reason = req.body.reason;
+const sumPoints = (userId, numberOfPoints, reason) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const userLevelRef = db.collection(collections.levels).doc(userId);
+            const userLevelGet = await userLevelRef.get();
+            const userLevel: any = userLevelGet.data();
 
-        const userLevelRef = db.collection(collections.levels).doc(userId);
-        const userLevelGet = await userLevelRef.get();
-        const userLevel: any = userLevelGet.data();
-
-        const historyPoint = {
-            points: numberOfPoints,
-            date: Date.now(),
-            reason: reason
-        }
-        if (userLevel) {
-            userLevel.points += numberOfPoints;
-            let updatedULHistoryPointsArray: any[] = [];
-            if (userLevel.historyPoints) {
-                let ulHistoryPoints = [...userLevel.historyPoints];
-                ulHistoryPoints.push(historyPoint);
-                updatedULHistoryPointsArray = ulHistoryPoints;
-            } else {
-                updatedULHistoryPointsArray.push(historyPoint);
+            const historyPoint = {
+                points: numberOfPoints,
+                date: Date.now(),
+                reason: reason
             }
-            userLevel.historyPoints = updatedULHistoryPointsArray;
+            if (userLevel) {
+                userLevel.points += numberOfPoints;
+                let updatedULHistoryPointsArray: any[] = [];
+                if (userLevel.historyPoints) {
+                    let ulHistoryPoints = [...userLevel.historyPoints];
+                    ulHistoryPoints.push(historyPoint);
+                    updatedULHistoryPointsArray = ulHistoryPoints;
+                } else {
+                    updatedULHistoryPointsArray.push(historyPoint);
+                }
+                userLevel.historyPoints = updatedULHistoryPointsArray;
+
+                let curLevel = LEVELS.length;
+                for (let i = 0; i < LEVELS.length; i++) {
+                    if (userLevel.points < LEVELS[i]) {
+                        curLevel = i + 1;
+                        break;
+                    }
+                }
+
+                await userLevelRef.update({
+                    level: curLevel,
+                    points: userLevel.points,
+                    historyPoints: updatedULHistoryPointsArray
+                });
+
+                resolve(userLevel);
+            } else {
+                let curLevel = LEVELS.length;
+                for (let i = 0; i < LEVELS.length; i++) {
+                    if (numberOfPoints < LEVELS[i]) {
+                        curLevel = i + 1;
+                        break;
+                    }
+                }
+
+                let userLevelNew = {
+                    userId: userId,
+                    level: curLevel,
+                    points: numberOfPoints,
+                    historyPoints: [historyPoint]
+                };
+
+                await db.runTransaction(async (transaction) => {
+                    transaction.set(db.collection(collections.levels).doc('' + userId), userLevelNew)
+                })
+
+                resolve(userLevelNew);
+            }
+        } catch (e) {
+            console.log('Error in controllers/userLevelsController -> sumPoints()', e);
+            reject({success: false, message: e});
+        }
+    });
+}
+
+const checkLevel = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const userLevelRef = db.collection(collections.levels).doc(userId);
+            const userLevelGet = await userLevelRef.get();
+            const userLevel: any = userLevelGet.data();
 
             let curLevel = LEVELS.length;
+            let pointsRemainingToLevelUp = 0;
             for (let i = 0; i < LEVELS.length; i++) {
                 if (userLevel.points < LEVELS[i]) {
                     curLevel = i + 1;
+                    pointsRemainingToLevelUp = LEVELS[i] - userLevel.points;
                     break;
                 }
             }
-
-            await userLevelRef.update({
-                level: curLevel,
-                points: userLevel.points,
-                historyPoints: updatedULHistoryPointsArray
-            });
-
-            res.send({
-                success: true, data: userLevel
-            })
-        } else {
-            let curLevel = LEVELS.length;
-            for (let i = 0; i < LEVELS.length; i++) {
-                if (numberOfPoints < LEVELS[i]) {
-                    curLevel = i + 1;
-                    break;
-                }
+            if (curLevel !== userLevel.level) {
+                await userLevelRef.update({
+                    level: curLevel
+                })
             }
-
-            let userLevelNew = {
-                userId: userId,
-                level: curLevel,
-                points: numberOfPoints,
-                historyPoints: [historyPoint]
-            };
-
-            await db.runTransaction(async (transaction) => {
-                transaction.set(db.collection(collections.levels).doc('' + userId), userLevelNew)
-            })
-
-            res.send({
-                success: true, data: userLevelNew
-            })
-        }
-    } catch (e) {
-        console.log('Error in controllers/userLevelsController -> sumPoints()', e);
-        res.send({
-            success: false,
-            message: e
-        });
-    }
-}
-
-exports.checkLevel = async (req: express.Request, res: express.Response) => {
-    try {
-        const userId = req.body.userId;
-
-        const userLevelRef = db.collection(collections.levels).doc(userId);
-        const userLevelGet = await userLevelRef.get();
-        const userLevel: any = userLevelGet.data();
-
-        let curLevel = LEVELS.length;
-        let pointsRemainingToLevelUp = 0;
-        for (let i = 0; i < LEVELS.length; i++) {
-            if (userLevel.points < LEVELS[i]) {
-                curLevel = i + 1;
-                pointsRemainingToLevelUp = LEVELS[i] - userLevel.points;
-                break;
-            }
-        }
-        if (curLevel !== userLevel.level) {
-            await userLevelRef.update({
-                level: curLevel
-            })
-        }
-
-        res.send({
-            success: true,
-            data: {
+            const res = {
                 level: curLevel,
                 points: userLevel.points,
                 pointsRemainingToLevelUp: pointsRemainingToLevelUp
-            }
-        })
-    } catch (e) {
-        console.log('Error in controllers/userLevelsController -> checkLevel()', e);
-        res.send({
-            success: false,
-            message: e
-        });
-    }
-}
-
-exports.pointsWonToday = async (req: express.Request, res: express.Response) => {
-    try {
-        const userId = req.body.userId;
-
-        const userLevelRef = db.collection(collections.levels).doc(userId);
-        const userLevelGet = await userLevelRef.get();
-        const userLevel: any = userLevelGet.data();
-        const oneDay = Date.now() + ONE_DAY;
-        const pointsSum = userLevel.historyPoints
-            .filter(hp => hp.date < oneDay)
-            .map(hp => hp.points)
-            .reduce((acc, points) => acc + points, 0);
-
-        res.send({
-            success: true,
-            data: {
-                pointsWon: pointsSum
-            }
-        })
-    } catch (e) {
-        console.log('Error in controllers/userLevelsController -> pointsWonToday()', e);
-        res.send({
-            success: false,
-            message: e
-        });
-    }
-}
-
-exports.getUserRank = async (req: express.Request, res: express.Response) => {
-    try {
-        const userId = req.params.userId;
-
-        const userLevelRef = db.collection(collections.levels).doc(userId);
-        const userLevelGet = await userLevelRef.get();
-        const userLevel: any = userLevelGet.data();
-
-        let points = userLevel.points;
-        const usersLevelRef = await db.collection(collections.levels)
-            .where("points", ">", points).get();
-        const userLevelRank = usersLevelRef.docs.length + 1;
-
-        res.send({
-            success: true,
-            data: {rank: userLevelRank}
-        });
-    } catch (e) {
-        console.log('Error in controllers/userLevelsController -> getUserRank()', e);
-        res.send({
-            success: false,
-            message: e
-        });
-    }
-}
-
-exports.getNumberOfUsersPerLevel = async (req: express.Request, res: express.Response) => {
-    try {
-        const amountOfLevels = LEVELS.length;
-        let usersPerLevels: number[] = []
-        for (let i = 1; i <= amountOfLevels; i++) {
-            const usersLevelRef = await db.collection(collections.levels)
-                .where("level", "==", i).get();
-            usersPerLevels.push(usersLevelRef.docs.length)
+            };
+            resolve(res);
+        } catch (e) {
+            console.log('Error in controllers/userLevelsController -> checkLevel()', e);
+            reject({success: false, message: e})
         }
+    });
+}
 
-        res.send({
-            success: true,
-            data: {usersPerLevels: usersPerLevels}
-        })
-    } catch (e) {
-        console.log('Error in controllers/userLevelsController -> getNumberOfUsersPerLevel()', e);
-        res.send({
-            success: false,
-            message: e
-        });
-    }
+const pointsWonToday = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const userLevelRef = db.collection(collections.levels).doc(userId);
+            const userLevelGet = await userLevelRef.get();
+            const userLevel: any = userLevelGet.data();
+            const oneDay = Date.now() + ONE_DAY;
+            const pointsSum = userLevel.historyPoints
+                .filter(hp => hp.date < oneDay)
+                .map(hp => hp.points)
+                .reduce((acc, points) => acc + points, 0);
+            resolve({pointsWon: pointsSum});
+        } catch (e) {
+            console.log('Error in controllers/userLevelsController -> pointsWonToday()', e);
+            reject({success: false, message: e})
+        }
+    });
+}
+
+const getUserRank = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const userLevelRef = db.collection(collections.levels).doc(userId);
+            const userLevelGet = await userLevelRef.get();
+            const userLevel: any = userLevelGet.data();
+
+            let points = userLevel.points;
+            const usersLevelRef = await db.collection(collections.levels)
+                .where("points", ">", points).get();
+            const userLevelRank = usersLevelRef.docs.length + 1;
+            resolve({rank: userLevelRank});
+        } catch (e) {
+            console.log('Error in controllers/userLevelsController -> getUserRank()', e);
+            reject({success: false, message: e})
+        }
+    });
+}
+
+const getNumberOfUsersPerLevel = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const amountOfLevels = LEVELS.length;
+            let usersPerLevels: number[] = []
+            for (let i = 1; i <= amountOfLevels; i++) {
+                const usersLevelRef = await db.collection(collections.levels)
+                    .where("level", "==", i).get();
+                usersPerLevels.push(usersLevelRef.docs.length)
+            }
+            resolve({usersPerLevels: usersPerLevels});
+        } catch (e) {
+            console.log('Error in controllers/userLevelsController -> getNumberOfUsersPerLevel()', e);
+            reject({success: false, message: e})
+        }
+    });
+}
+
+module.exports = {
+    sumPoints,
+    checkLevel,
+    pointsWonToday,
+    getUserRank,
+    getNumberOfUsersPerLevel
 }
--- src/routes/userLevelsRoutes.ts
+++ src/routes/userLevelsRoutes.ts
@@ -5,10 +5,5 @@ const router = express.Router();
 const userLevelsController = require('../controllers/userLevelsController');
 
 router.get('/getInfo', authenticateJWT, userLevelsController.getLevelsInfo);
-router.post('/sumPoints', authenticateJWT, userLevelsController.sumPoints);
-router.post('/checkLevel', authenticateJWT, userLevelsController.checkLevel);
-router.post('/pointsWonToday', authenticateJWT, userLevelsController.pointsWonToday);
-router.get('/getUserRank', authenticateJWT, userLevelsController.getUserRank);
-router.get('/getNumberOfUsersPerLevel', authenticateJWT, userLevelsController.getNumberOfUsersPerLevel);
 
 module.exports = router;
