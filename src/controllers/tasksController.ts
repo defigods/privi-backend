57627117fdb405862f3a6e83de06220edfc551fc
defigods <defigodss@gmail.com>
2021-02-02 23:41:29 +0100

    Tasks controller users update

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -13,68 +13,55 @@ exports.getTasks = async (req: express.Request, res: express.Response) => {
 
     const allTasks = await db.collection(collections.tasks).get();
 
-    let userTasks = [] as any;
+    let userTasks = user.UserTasks || ([] as any);
     let userTasksToDisplay = [] as any;
-    //1. check if user has tasks
 
-    //if user has already tasks
-    if (user.UserTasks && user.UserTasks.length > 0) {
-      //load them
-      allTasks.docs.map((doc, i) => {
-        if (
-          doc.data().Level <= user.level &&
-          !user.UserTasks.some((task) => task.Id === doc.id)
-        ) {
-          let task = { Id: doc.id, Completed: false } as any;
-          if (doc.data().Timed) {
-            task.Timed = doc.data().Timed;
-            //TODO: set timing rules ??
-            //.EndDate and .StartDate
-          }
-          userTasks.push(task);
+    allTasks.docs.map(async (doc, i) => {
+      //load tasks if user has the level task or higher &&
+      //user has no tasks OR user doesn't already have this task
+      if (
+        doc.data().Level <= user.level &&
+        (user.UserTasks === undefined ||
+          (user.UserTasks &&
+            !user.UserTasks.some((task) => task.Id === doc.id)))
+      ) {
+        let task = { Id: doc.id, Completed: false } as any;
+        if (doc.data().Timed) {
+          task.Timed = doc.data().Timed;
+          //TODO: set timing rules ??
+          //.EndDate and .StartDate
         }
-        if (userTasks.some((task) => task.Id === doc.id)) {
-          let taskTodisplay = { ...doc.data() };
+        userTasks.push(task);
+      }
 
-          taskTodisplay.Completed = userTasks.findIndex(
-            (task) => task.Id === doc.id
-          ).Completed;
-          userTasksToDisplay.push(taskTodisplay);
-        }
-      });
+      //add user to task
+      if (
+        doc.data().Users &&
+        !doc.data().Users.some((user) => user === userId)
+      ) {
+        const usersCopy = doc.data().Users || [];
+        usersCopy.push(userId);
 
-      //and update db
-      await db.collection(collections.tasks).doc(userId).update({
-        UserTasks: userTasks,
-      });
-    } else {
-      //if user has no tasks registered, load all tasks and create the collection
-      //push tasks
-      allTasks.docs.map((doc, i) => {
-        if (doc.data().Level <= user.level) {
-          let task = { Id: doc.id, Completed: false } as any;
-          if (doc.data().Timed) {
-            task.Timed = doc.data().Timed;
-            //TODO: set timing rules ??
-            //.EndDate and .StartDate
-          }
-          userTasks.push(task);
+        await db.collection(collections.tasks).doc(doc.id).update({
+          Users: usersCopy,
+        });
+      }
 
-          if (userTasks.some((task) => task.Id === doc.id)) {
-            let taskTodisplay = { ...doc.data() };
-            taskTodisplay.Completed =
-              userTasks[
-                userTasks.findIndex((task) => task.Id === doc.id)
-              ].Completed;
-            userTasksToDisplay.push(taskTodisplay);
-          }
-        }
-      });
-      //and update db
-      await db.collection(collections.user).doc(userId).update({
-        UserTasks: userTasks,
-      });
-    }
+      //and add info to the list to be returned
+      if (userTasks.some((task) => task.Id === doc.id)) {
+        let taskTodisplay = { ...doc.data() };
+
+        taskTodisplay.Completed = userTasks.findIndex(
+          (task) => task.Id === doc.id
+        ).Completed;
+        userTasksToDisplay.push(taskTodisplay);
+      }
+    });
+
+    //and update db
+    await db.collection(collections.user).doc(userId).update({
+      UserTasks: userTasks,
+    });
 
     if (userTasksToDisplay.length > 0) {
       res.send({ success: true, data: userTasksToDisplay });
