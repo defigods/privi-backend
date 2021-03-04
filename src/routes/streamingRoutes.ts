90fc5f5ecadef1a439488c5a0f34a91b0075a8f3
ninjadefi <ninjadefi007@gmail.com>
2021-03-04 20:25:30 +0100

    feat: add endVideoStreaming

--- src/routes/streamingRoutes.ts
+++ src/routes/streamingRoutes.ts
@@ -7,6 +7,7 @@ const streamingController = require("../controllers/streamingController");
 // called from postman
 router.post("/initiateStreaming", authenticateJWT, streamingController.initiateStreaming)
 router.post("/createVideoStreaming", authenticateJWT, streamingController.createVideoStreaming)
+router.post("/endVideoStreaming", authenticateJWT, streamingController.endVideoStreaming)
 
 
 module.exports = router;
