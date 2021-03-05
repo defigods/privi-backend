dbacc36cc8e0416ce5af3eff798da39aab1fee59
ninjadefi <ninjadefi007@gmail.com>
2021-03-05 21:35:59 +0100

    feat: add scheduleVideoStreaming and update the logic for createVideoStreaming

--- src/routes/streamingRoutes.ts
+++ src/routes/streamingRoutes.ts
@@ -7,6 +7,7 @@ const streamingController = require("../controllers/streamingController");
 // called from postman
 router.post("/initiateStreaming", authenticateJWT, streamingController.initiateStreaming)
 router.post("/createVideoStreaming", authenticateJWT, streamingController.createVideoStreaming)
+router.post("/scheduleVideoStreaming", authenticateJWT, streamingController.scheduleVideoStreaming)
 router.post("/endVideoStreaming", authenticateJWT, streamingController.endVideoStreaming)
 router.get("/listStreaming", authenticateJWT, streamingController.listStreaming)
 
