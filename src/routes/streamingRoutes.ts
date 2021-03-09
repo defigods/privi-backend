4fe62614c5f78e8d9d4c2c9a62a3ceb4a8da1b4e
Yimin <defigodss@gmail.com>
2021-03-09 18:17:02 +0100

    streaming routes

--- src/routes/streamingRoutes.ts
+++ src/routes/streamingRoutes.ts
@@ -5,11 +5,13 @@ import { authenticateJWT } from "../middlewares/jwtAuthMiddleware";
 const streamingController = require("../controllers/streamingController");
 
 // called from postman
-router.post("/initiateStreaming", authenticateJWT, streamingController.initiateStreaming)
 router.post("/initiateMediaLiveStreaming", authenticateJWT, streamingController.initiateMediaLiveStreaming)
+router.post("/exitMediaLiveStreaming", authenticateJWT, streamingController.exitMediaLiveStreaming)
 router.post("/createVideoStreaming", authenticateJWT, streamingController.createVideoStreaming)
-router.post("/scheduleVideoStreaming", authenticateJWT, streamingController.scheduleVideoStreaming)
+
+router.post("/initiateStreaming", authenticateJWT, streamingController.initiateStreaming)
 router.post("/endVideoStreaming", authenticateJWT, streamingController.endVideoStreaming)
+router.post("/scheduleVideoStreaming", authenticateJWT, streamingController.scheduleVideoStreaming)
 router.get("/listStreaming", authenticateJWT, streamingController.listStreaming)
 
 
