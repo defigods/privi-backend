69963890874102ae9300cd8f9457d09bc3b6b32c
Ben <superman.star25@gmail.com>
2021-04-15 10:20:58 -0400

    add like controller in collab

--- src/routes/collabRoutes.ts
+++ src/routes/collabRoutes.ts
@@ -8,6 +8,7 @@ const collabController = require('../controllers/collabController');
 router.post('/createCollab', authenticateJWT, collabController.createCollab);
 router.post('/upvote', authenticateJWT, collabController.upvote);
 router.post('/react', authenticateJWT, collabController.react);
+router.post('/like', authenticateJWT, collabController.like);
 
 router.get('/getCollabs', authenticateJWT, collabController.getCollabs);
 router.get('/getTwitterUsers', collabController.getTwitterUsers);
