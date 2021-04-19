de40ca666c15892b369c63a61ccefcddab5e1e47
defigods <defigodss@gmail.com>
2021-04-19 11:27:38 +0100

    Extend bodyparser urlencoded

--- src/controllers/serverController.ts
+++ src/controllers/serverController.ts
@@ -73,7 +73,7 @@ export const startServer = (env: Env) => {
   // Configure Express to use EJS
   app.set("views", path.join(__dirname, "views"));
   app.use(express.json({ limit: '50mb' }));
-  app.use(express.urlencoded({ limit: '50mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
   // Routes definition
   app.use("/user", userRoutes);
