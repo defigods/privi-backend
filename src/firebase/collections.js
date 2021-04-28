44763148588bf658745782eb625f58e56b31bfea
defigods <defigodss@gmail.com>
2021-04-28 19:02:09 +0200

    mediaOnCommunityChat -> started

--- src/firebase/collections.js
+++ src/firebase/collections.js
@@ -166,6 +166,7 @@ module.exports = {
   mediaFraction: 'MediaFraction', // Fractionalise
   ownershipHistory: 'OwnershipHistory',
   mediaOnCommunity: 'MediaOnCommunity',
+  mediaOnCommunityChat: 'MediaOnCommunityChat',
 
   playList: 'PlayList',
 
