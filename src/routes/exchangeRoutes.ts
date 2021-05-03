ea8c4558da615a89a7237e5af63d94203394fd89
defigods <defigodss@gmail.com>
2021-05-03 15:40:25 +0200

    exchange getter functions

--- src/routes/exchangeRoutes.ts
+++ src/routes/exchangeRoutes.ts
@@ -6,11 +6,14 @@ import { authenticateJWT } from '../middlewares/jwtAuthMiddleware';
 const exchangeController = require('../controllers/exchangeController');
 
 router.post('/createExchange', authenticateJWT, exchangeController.createExchange);
-router.post('/placeBuyingOffer', authenticateJWT, exchangeController.placeBuyingOffer);
+router.post('/placeBuyingOffer', exchangeController.placeBuyingOffer);
 router.post('/placeSellingOffer', authenticateJWT, exchangeController.placeSellingOffer);
 router.post('/buyFromOffer', authenticateJWT, exchangeController.buyFromOffer);
 router.post('/sellFromOffer', authenticateJWT, exchangeController.sellFromOffer);
 router.post('/cancelBuyingOffer', authenticateJWT, exchangeController.cancelBuyingOffer);
 router.post('/cancelSellingOffer', authenticateJWT, exchangeController.cancelSellingOffer);
+
+router.get('/getPriceHistory/:exchangeId', exchangeController.getPriceHistory);
+router.get('/getBuyingOffers/:exchangeId', exchangeController.getBuyingOffers);
 
 module.exports = router;
