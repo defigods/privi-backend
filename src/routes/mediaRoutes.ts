419d0ec8fba273dbb452a78280c289a8535b2ea5
defigods <defigodss@gmail.com>
2021-05-04 20:05:54 +0200

    getplaylist update (authentication not required)

--- src/routes/mediaRoutes.ts
+++ src/routes/mediaRoutes.ts
@@ -115,7 +115,7 @@ router.get('/getMediaMainPhoto/:mediaId', mediaController.getMediaMainPhoto);
 
 router.get('/getPlaylists', playlistController.getPlaylists);
 router.get('/getMyPlaylists/:userId', authenticateJWT, playlistController.getMyPlaylists);
-router.get('/getPlaylist/:playListId', authenticateJWT, playlistController.getPlaylist);
+router.get('/getPlaylist/:playListId', playlistController.getPlaylist);
 router.post('/likePlaylist/:playlistId', authenticateJWT, playlistController.likePlaylist);
 router.post('/removeLikePlaylist/:playlistId', authenticateJWT, playlistController.removeLikePlaylist);
 
